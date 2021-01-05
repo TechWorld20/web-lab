2bdd1b263604d8358235db7843b395b9ed8f86f2
manoranjini <72066405+TechWorld20@users.noreply.github.com>
2021-01-05 11:14:43 +0530

    Add files via upload

---
+++ lab pro7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1"/>
+<style>
+p {
+color:white;
+font-size:50px;
+position: absolute;
+top: 30%;
+left: 30%;
+transform: translate(-50%, -50%);
+}
+body{background-color:black;}
+</style>
+<p> <?php echo date(" h: i : s A");?> </p>
+</head>
